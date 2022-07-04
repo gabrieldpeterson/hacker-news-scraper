02ec2bdc194698b4052436ea94f5cae1457723bd
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-07-04 13:48:35 -0700

    Have the results returned in order of votes by highest to lowest

--- scrape.py
+++ scrape.py
@@ -3,6 +3,10 @@ from bs4 import BeautifulSoup
 import pprint
 
 
+def sort_by_votes(hnlist):
+    return sorted(hnlist, key=lambda x: x['votes'], reverse=True)
+
+
 def create_custom_hn(links, subtext, min_points=0):
     """Create custom dictionary that contains title, link, and points for anything above a minimum amount of points"""
     hn = []
@@ -14,7 +18,7 @@ def create_custom_hn(links, subtext, min_points=0):
             points = int(vote[0].getText().replace(' points', ''))
             if points >= min_points:
                 hn.append({'title': title, 'link': href, 'votes': points})
-    return hn
+    return sort_by_votes(hn)
 
 
 res = requests.get('https://news.ycombinator.com/news')
