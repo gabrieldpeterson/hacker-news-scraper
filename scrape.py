492383274e80aa6bdaafe612db4d04e64020a4df
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-07-04 13:02:35 -0700

    Create a function that returns a dictionary of the scraped headlines, links, and points

--- scrape.py
+++ scrape.py
@@ -1,9 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 
-def create_custom_hn(links, votes):
+def create_custom_hn(links, subtext, min_points=0):
+    """Create custom dictionary that contains title, link, and points for anything above a minimum amount of points"""
     hn = []
+    for index, item in enumerate(links):
+        title = links[index].getText()
+        href = links[index].get('href', None)
+        vote = subtext[index].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points >= min_points:
+                hn.append({'title': title, 'link': href, 'votes': points})
     return hn
 
 
@@ -11,11 +21,8 @@ res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 # print(soup.select('.titlelink')[0].text)
 links = soup.find_all(attrs={'class': 'titlelink'})
-votes = soup.select('.score')
+subtext = soup.select('.subtext')
 found_votes = links
 
-print(len(links), len(votes))
-
-for link in links:
-    print(link.getText)
+pprint.pprint(create_custom_hn(links, subtext, 100))
 
